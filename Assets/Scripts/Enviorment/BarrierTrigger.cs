5ccb3a9e516c1b6ef51e45fa776c205b3c33cd85
Lucas <lucaspietruschka@gmx.de>
2026-02-16 22:10:11 +0100

    ADD: Projecticle  prefabs & Skeleton Items

--- Assets/Scripts/Enviorment/BarrierTrigger.cs
+++ Assets/Scripts/Enviorment/BarrierTrigger.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 public class BarrierTrigger : MonoBehaviour
 {
     [SerializeField] private GameObject barrier;
+    [SerializeField] private GameObject boss;
 
     private void OnTriggerEnter(Collider other)
     {
         PlayerActions player = other.GetComponent<PlayerActions>();
         if(player != null)
             barrier.gameObject.SetActive(true);
+            boss.gameObject.SetActive(true);
     }
 }
