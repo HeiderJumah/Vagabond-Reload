77c0df91bd3540172e5a013ee38a5ca41fe9360b
Lucas <lucaspietruschka@gmx.de>
2026-02-26 04:19:59 +0100

    ADD: LevelThree design; FIX: Test weapons not working when player is originally from a different scene

--- Assets/Scripts/Player/WeaponTest.cs
+++ Assets/Scripts/Player/WeaponTest.cs
@@ -4,16 +4,9 @@ using UnityEngine.InputSystem;
 
 public class WeaponTest : MonoBehaviour
 {
-    private PlayerActions playerActions;
     [SerializeField] private WeaponType weaponType;
-
-    private void Start()
-    {
-        playerActions = FindFirstObjectByType<PlayerActions>();
-    }
-
     public void Interact()
     {
-        playerActions.EquipWeapon(weaponType);        
+        PlayerActions.Instance.EquipWeapon(weaponType);        
     }
 }
